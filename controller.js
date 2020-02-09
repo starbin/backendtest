985d2002fb156b29fbd638ee61890d897797c05d
bintang raga <>
2020-02-10 00:51:25 +0700

    first commit

---
+++ controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./conn');
+
+exports.users = function(req, res) {
+    connection.query('SELECT * FROM person', function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.userId = function(req, res){
+    connection.query('SELECT'+res+'FROM person', function (error, rows, fields){
+        if(error){
+            console.log(error);
+            
+        }else(
+            response.ok(rows, res)
+        )
+    })
+}
+
+exports.index = function(req, res) {
+    response.ok("Hello from the Node JS RESTful side!", res)
+};
---
+++ routes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function(app) {
+    var todoList = require('./controller');
+
+    app.route('/')
+        .get(todoList.index);
+
+    app.route('/users')
+        .get(todoList.users);
+
+    app.route('/users/', id)
+        .get(todoList.userId)
+};
